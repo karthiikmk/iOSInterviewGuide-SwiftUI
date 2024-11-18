0a24c492a45216038b7773ae2eb8d9673befaff3
Karthik <mkkarthiik@gmail.com>
2024-11-18 19:50:48 +0530

    Initial Commit

---
+++ iOSInterviewGuide-SwiftUI/iOSInterviewGuide_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSInterviewGuide_SwiftUIApp.swift
+//  iOSInterviewGuide-SwiftUI
+//
+//  Created by Karthik on 18/11/24.
+//
+
+import SwiftUI
+
+@main
+struct iOSInterviewGuide_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
